8f60b9237b5e8fe58019a3d1accfa4fc8498e059
vasiljeu95 <vasilyeu95@gmail.com>
2019-11-25 23:49:08 +0300

    Initial Commit

---
+++ Log In View App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Log In View App
+//
+//  Created by Stepan Vasilyeu on 11/25/19.
+//  Copyright © 2019 Stepan Vasilyeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
